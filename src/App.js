9730e76c22e52d70f346ee95c6c84043a9f1b56f
Khanh Tran-Cong <congkhanh197@gmail.com>
2017-07-08 11:25:29 +0700

    fix to run

--- src/App.js
+++ src/App.js
@@ -1,13 +1,6 @@
-import React, {
-  Component
-} from 'react';
+import React, { Component } from 'react';
 import './App.css';
-import { createStore } from 'redux'
-//import App from './components/App'
-import reducer from './reducers'
 
-let store
- = createStore(reducer)
 
 class App extends Component {
   constructor(props) {
